dd549ce630f9e0d358f6095603bfb52450a097c7
febrian <febyianandy@gmai.com>
2022-03-24 21:43:32 +0700

    assigment function javascript

--- frontend/javascript/6-function-cp/script-1.js
+++ frontend/javascript/6-function-cp/script-1.js
@@ -7,9 +7,16 @@
 
 function reverseString(str) {
     // TODO: answer here
+    var i = str.length,
+      o = '';
+    while (i > 0) {
+        o += str.substring(i - 1, i);
+        i--;
+    }
+    return o;
 }
 
-const string = prompt('Masukan teks: ');
+// const string = prompt('Masukan teks: ');
 
-const result = reverseString(string);
+const result = reverseString('Hello world');
 console.log(result);
--- frontend/javascript/6-function-cp/script-2.js
+++ frontend/javascript/6-function-cp/script-2.js
@@ -9,14 +9,25 @@
 // teks2 dibalik sama menjadi madam, maka balikan akan true
 // teks3 dibalik sama menjadi kasur ini rusak, maka balikan akan true
 
-function checkPalindrome(string) {
+function checkPalindrome(str) {
     // TODO: answer here
+    var re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;    
 }
 
+
+
 // masukan teks
-const string = prompt('Masukan string: ');
+// const string = prompt('Masukan string: ');
 
 // memanggil fungsi palindrom
-const value = checkPalindrome(string);
+const value = checkPalindrome('A man, a plan, a canal. Panama');
 
 console.log(value);
