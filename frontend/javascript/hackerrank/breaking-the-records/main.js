59e3edab49f9953236df443d0f7996e2aaa93e9a
febrian <febyianandy@gmai.com>
2022-04-01 09:05:20 +0700

    update breaking-the-records

--- frontend/javascript/hackerrank/breaking-the-records/main.js
+++ frontend/javascript/hackerrank/breaking-the-records/main.js
@@ -12,9 +12,25 @@
 
 // Full Problem: https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
 
-function breakingRecords(scores) {
+function breakingRecords(arr) {
   // Write your code here
   // TODO: answer here
+  let best;
+    let worst;
+    best = worst = arr.shift() //Get and remove the first element
+    let n = arr.length;
+    let recBest = 0;  //Use more descriptive names here
+    let recWorst = 0; //...and here as well
+    for (let i = 0; i < n; i++) {
+        if (arr[i] > best) {
+            best = arr[i];
+            recBest++;
+        } else if (arr[i] < worst) {
+            worst = arr[i];
+            recWorst++;
+        }
+    }
+    return [recBest, recWorst];
 }
 
 function main() {
